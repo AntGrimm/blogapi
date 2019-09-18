ae19f102f11b18a5dc99a39ae737fe815225b39d
Tony Grimm <anthony.l.grimm@gmail.com>
2019-09-18 11:47:33 -0400

    added GET 1, refactored

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using blogapi;
 using BlogApi.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -8,17 +9,49 @@ namespace BlogApi.Controllers
   [ApiController]
   public class BlogController : ControllerBase
   {
+    private DatabaseContext context;
+
+    public BlogController()
+    {
+      this.context = new DatabaseContext();
+    }
+
     [HttpPost]
     public ActionResult<Blog> CreateEntry([FromBody]Blog entry)
     {
-      // add entry to the database
       // 1. reference the database
-      var context = new DatabaseContext();
+      //var context = new DatabaseContext();
       // 2. do the thing
       context.Blogs.Add(entry);
       // 3. save the thing
       context.SaveChanges();
       return entry;
+    }
+
+    // [HttpGet]
+    // public ActionResult<IEnumberable<Blog>> GetAllBlogs()
+    // {
+    //   // 1. reference the database
+    //   var context = new DatabaseContext();
+    //   // 2. do the thing
+    //   var blogs = context.Blogs.OrderByDescending(blogapi => blogapi.DateCreated);
+    //   // 3. return the thing
+    //   return blogs.ToList();
+    // }
+
+    [HttpGet("{id}")]
+    public ActionResult<Blog> GetOneBlog(int id)
+    {
+      //var context = new DatabaseContext();
+      var blog = context.Blogs.FirstOrDefault(b => b.Id == id);
+      if (blog == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        return Ok(blog);
+      }
     }
   }
 }
